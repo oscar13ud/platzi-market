9b725337d8901a3ede933b92bd59fd5255870df9
Oscar Diaz <oscar13@udenar.edu.co>
2021-12-18 08:44:44 -0500

    primer commit

---
+++ src/main/java/complatzimarket/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package complatzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/complatzimarket/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package complatzimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
